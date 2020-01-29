e12a98b52ee5d75002410e3f6097751d7a4238ef
Anna Shadrina <ansshadrina@gmail.com>
2020-01-29 21:22:57 +0300

    Added new class Circle

---
+++ ProjectRed/Lessons/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRed.Lessons
+{
+    public class Circle : Figure
+    {
+        private double radius;
+
+        public Circle(double x, double y) : base(x, y)
+        {
+
+        }
+    }
+}
