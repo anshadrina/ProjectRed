74afdfd1b5e3482fe6cf39c4dc3ce1adf753ca71
Anna Shadrina <ansshadrina@gmail.com>
2020-01-29 23:30:57 +0300

    Added new classes Warrior, WarriorHardArmor, WarriorLightArmor.

---
+++ ProjectRed/Lessons/Warrior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRed.Lessons
+{
+    public class Warrior
+    {
+        private double currentAmountLives;
+        private double maxAmountLives = 500;
+        public double CurrentAmountLives
+        {
+            get
+            {
+                return currentAmountLives;
+            }
+            protected set
+            {
+                if (value < 0)
+                    currentAmountLives = 0;
+                else if (value > maxAmountLives)
+                    currentAmountLives = maxAmountLives;
+                else currentAmountLives = value;
+            }
+        }
+
+        public Warrior()
+        {
+            currentAmountLives = 100;
+        }
+
+        public Warrior(double currentAmountLives)
+        {
+            this.currentAmountLives = currentAmountLives;
+        }
+
+        public virtual void GetDamage(double damage)
+        {
+            CurrentAmountLives -= damage; 
+        }
+    }
+}
